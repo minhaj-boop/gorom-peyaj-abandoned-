753f91826893950941515a6d6f47f070f23734f6
minhaj-boop <imminhajj@gmail.com>
2022-01-16 15:28:33 +0600

    lul

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Banner from "./Pages/Home/Banner/Banner";
+import Header from "./Pages/Shared/Header/Header";
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <Banner></Banner>
     </div>
   );
--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import './Banner.css';
 import bannerImg from '../../../images/bannerbackground.png';
-import { Box } from '@mui/material';
-
 
 
 const Banner = () => {
     return (
-        <Box sx={{ width: 1 }}>
-            <div className="boxx">
-                <div>
-                    <h1 className="txt-position">Banner</h1>
-                    <img src={bannerImg} alt="" />
-                </div>
-            </div>
-        </Box>
+        <div className="div-position">
+            <h1 className="txt-position">Best Food Waiting For Your Tummy</h1>
+            <img src={bannerImg} alt="" />
+        </div>
     );
 };
 
